10839b956b8bc1c8ecc4cadfc6809bfe9e6d1f49
Sahil Shrestha <35665722+sahilian@users.noreply.github.com>
2023-12-24 14:05:09 +0545

    Implementation of AutoMapper

--- obj/Debug/net6.0/Task2.AssemblyInfo.cs
+++ obj/Debug/net6.0/Task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+955b12632984cd94e679e54df040fceb1b3a2069")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
